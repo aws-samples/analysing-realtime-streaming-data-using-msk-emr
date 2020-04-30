240edf6d43f887b00b0a5767060d5433ff8a39a3
swarajy71 <64226084+swarajy71@users.noreply.github.com>
2020-04-30 15:29:17 +0530

    Add files via upload

---
+++ install-scripts.sh
@@ -0,0 +1,13 @@
+wget http://archive.apache.org/dist/kafka/1.1.1/kafka_2.11-1.1.1.tgz
+wget https://archive.apache.org/dist/kafka/2.1.0/kafka_2.11-2.1.0.tgz
+
+tar -xzf kafka_2.11-2.1.0.tgz
+
+wget http://central.maven.org/maven2/org/apache/kafka/kafka_2.10/0.10.0.0/kafka_2.10-0.10.0.0.jar
+
+wget https://archive.apache.org/dist/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz
+
+tar -xzf kafka_2.11-0.10.0.0.tgz
+
+
+wget http://central.maven.org/maven2/org/apache/spark/spark-streaming-kafka-0-10_2.11/2.4.0/spark-streaming-kafka-0-10_2.11-2.4.0.jar
